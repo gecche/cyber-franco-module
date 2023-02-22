2eb9cfde7a7ee47a51676b58fc849774079e92e0
Giacomo Terreni <giacomoterreni@gmail.com>
2023-02-22 14:23:49 +0100

    some refactoring (3)

--- Console/Commands/AddFrancoBackpackRoute.php
+++ Console/Commands/AddFrancoBackpackRoute.php
@@ -5,22 +5,21 @@ namespace modules\CyberFranco\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-class AddCustomRouteContent extends Command
+class AddFrancoBackpackRoute extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'backpack:add-custom-route
-                                {code : HTML/PHP code that registers a route. Use either single quotes or double quotes. Never both. }';
+    protected $signature = 'franco:add-backpack-route';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Add HTML/PHP code to the routes/backpack/custom.php file';
+    protected $description = 'Add Franco backpack route if needed';
 
     /**
      * Create a new command instance.
